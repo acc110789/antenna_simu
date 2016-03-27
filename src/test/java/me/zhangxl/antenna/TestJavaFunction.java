6ea6560d653eebec4f30d14aa64b7ad04b03356b
zhangxl <zhangxl@corp.jadenine.com>
2016-03-27 14:16:11 +0800

    添加testCase

--- src/test/java/me/zhangxl/antenna/TestJavaFunction.java
+++ src/test/java/me/zhangxl/antenna/TestJavaFunction.java
@@ -1,19 +1,81 @@
 package me.zhangxl.antenna;
 
 
-import org.junit.Assert;
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.charset.Charset;
+import java.util.Random;
 
 /**
  * Created by zhangxiaolong on 16/3/26.
  */
-public class TestJavaFunction  {
+public class TestJavaFunction {
 
     @Test
-    public void addFunction(){
-        int i=3;
-        int j= 4;
-        Assert.assertEquals(i+1,j);
+    public void shortShift() {
+        short cc = -1;
+        System.out.println(Integer.toBinaryString(cc));
+        cc >>>= 5;
+        System.out.println(cc);
+        System.out.println(Integer.toBinaryString(cc));
+        System.out.println((short) 100000000);
+        System.out.println(Charset.defaultCharset().displayName());
+        System.out.println("it's 知乎日报".length());
+        System.out.println("日".getBytes(Charset.forName("unicode")).length);
+        System.out.println("it's 知乎日报".getBytes(Charset.forName("utf-8")).length);
+        System.out.println("it's 知乎日报".getBytes(Charset.forName("gbk")).length);
+        System.out.println("it's 知乎日报".getBytes(Charset.forName("ascii")).length);
     }
+
+    @Test
+    public void randomAccessFile() throws IOException {
+        File file = File.createTempFile("zhangxl", "tmp");
+        RandomAccessFile rs = null;
+        try {
+            rs = new RandomAccessFile(file, "rw");
+            Random random = new Random(System.currentTimeMillis());
+            for (int i = 0; i < 5; i++) {
+                double value = random.nextDouble();
+                rs.writeDouble(value);
+            }
+        } finally {
+            IOUtils.closeQuietly(rs);
+        }
+        System.out.println("first display");
+        display(file);
+        try {
+            rs = new RandomAccessFile(file, "rw");
+            rs.seek(2 * 8);
+            rs.writeDouble(12.333343333333);
+        } finally {
+            IOUtils.closeQuietly(rs);
+        }
+        System.out.println("second display");
+        display(file);
+    }
+
+    private void display(File file) {
+        RandomAccessFile rs = null;
+        try {
+            rs = new RandomAccessFile(file, "r");
+            while (true) {
+                try {
+                    System.out.println(rs.readDouble());
+                } catch (IOException e) {
+                    return;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            IOUtils.closeQuietly(rs);
+        }
+    }
+
 
 }
